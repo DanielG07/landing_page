d196716993fad8d756d687ea310121e131e36937
Angel Sanchez <asanchezg1507@alumno.ipn.mx>
2024-11-05 13:11:21 -0600

    fix npm run build

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,30 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
 
-import Nav from '@components/Nav';
-import Footer from '@components/Footer';
+import Nav from "@components/Nav";
+import Footer from "@components/Footer";
 //import Provider from '@components/Provider'
 
 export const metadata = {
   title: "Xcript CS",
-  description: 'Cloud and Blockchain Solutions',
-}
+  description: "IA & Cloud Solutions",
+};
 
-const Rootlayout = ({children}) => {
+const Rootlayout = ({ children }) => {
   return (
-    <html lang='es'>
+    <html lang="es">
       <body>
-        <div className='main'>
-          <div className='gradient'></div>
+        <div className="main">
+          <div className="gradient"></div>
         </div>
 
-        <main className='app'>
-          <Nav/>
+        <main className="app">
+          <Nav />
           {children}
-          <Footer/>
+          <Footer />
         </main>
       </body>
     </html>
-  )
-}
+  );
+};
 
 export default Rootlayout;
